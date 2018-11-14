d327f5f870c9be14464a18a8c806a8b9699f29d8
Anwesh43 <anweshthecool0@gmail.com>
2018-11-14 09:49:09 +0530

    adding canvas extension method drawBCRSNode

--- bicirclerotstepview/src/main/java/com/anwesh/uiprojects/bicirclerotstepview/BiCircleRotStepView.kt
+++ bicirclerotstepview/src/main/java/com/anwesh/uiprojects/bicirclerotstepview/BiCircleRotStepView.kt
@@ -30,6 +30,40 @@ fun Int.getMirrorMultiplier(k : Float) : Float = this * (1 - k) + k
 
 fun Float.updateScale(dir : Float, a : Int) : Float = scGap * dir * a.getMirrorMultiplier(getScaleFactor())
 
+fun Int.getYFactor() : Int = this / 2
+
+fun Int.getXFactor()  : Int = this % 2
+
+fun Int.getRevFactor()  : Int = (this.getYFactor() + this.getXFactor()).getXFactor()
+
+fun Canvas.drawBCRSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / 3
+    val r : Float = size / 2
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / STROKE_FACTOR
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = Color.parseColor("#0D47A1")
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(180f * sc2)
+    for(j in 0..(parts-1)) {
+        val sc : Float = sc1.divideScale(j, parts)
+        val degFactor : Int = j.getYFactor()
+        val rFactor : Int = j.getRevFactor()
+        val deg : Float = 180f + 180f * degFactor
+        save()
+        translate(-r + 2 * r * rFactor, 0f)
+        drawArc(RectF(-r, -r, r, r), deg - 180f * sc, 180f * sc, false, paint)
+        restore()
+    }
+    restore()
+}
+
 class BiCircleRotStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
