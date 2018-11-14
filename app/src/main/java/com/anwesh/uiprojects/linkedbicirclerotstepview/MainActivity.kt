32afe9f4e9f36be1c2eca9b02f254880a4942440
Anwesh43 <anweshthecool0@gmail.com>
2018-11-14 10:22:16 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbicirclerotstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbicirclerotstepview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedbicirclerotstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.bicirclerotstepview.BiCircleRotStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view : BiCircleRotStepView = BiCircleRotStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
