9e638ce6987b1737c2426b74e27cc2fc1165fca2
Anwesh43 <anweshthecool0@gmail.com>
2018-11-14 08:55:48 +0530

    importing libraries and defining constants

--- bicirclerotstepview/src/main/java/com/anwesh/uiprojects/bicirclerotstepview/BiCircleRotStepView.kt
+++ bicirclerotstepview/src/main/java/com/anwesh/uiprojects/bicirclerotstepview/BiCircleRotStepView.kt
@@ -3,3 +3,17 @@ package com.anwesh.uiprojects.bicirclerotstepview
 /**
  * Created by anweshmishra on 14/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val parts : Int = 4
+val STROKE_FACTOR : Int = 60
+val SIZE_FACTOR : Int = 3
